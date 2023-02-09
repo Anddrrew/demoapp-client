e9ced9ed249c352eacb9b57e9b2a64379644c1f3
Anddrrew <59182007+Anddrrew@users.noreply.github.com>
2023-02-09 22:58:43 +0200

    Added profile template

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,17 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { Button, Typography } from '@mui/material';
-import { appPaths } from '../router/paths';
-import AppLink from '../components/Link';
+import { Avatar, Card, CardContent, CardMedia, Grid, Stack, Typography } from '@mui/material';
 
 export default function Profile() {
-  const { user, logout } = useAuth0();
+  const { user } = useAuth0();
 
   return (
-    <>
-      <Typography>Profile</Typography>
-      <AppLink to={appPaths.INDEX}>Go to home</AppLink>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <Button onClick={() => logout()}>Log Out</Button>
-    </>
+    <Grid container justifyContent={'center'}>
+      <Grid item>
+        <Card variant='outlined'>
+          <CardMedia sx={{ padding: 2 }}>
+            <Avatar src={user?.picture} sx={{ width: 294, height: 294 }} />
+          </CardMedia>
+          <CardContent>
+            <Stack spacing={1}>
+              <Typography variant={'h6'}>{user?.nickname}</Typography>
+              <Typography>{user?.email}</Typography>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
